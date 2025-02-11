ac23a0ccd80ae4d1fae95e0213cb9a053001aa3c
madhukar4488 <madhukardarra4488@gmail.com>
2025-02-11 12:50:15 +0530

    Sort an array without using predefined method

---
+++ Demo074.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Demo074 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        scanner.close();
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j] + arr[j + 1];
+                    arr[j + 1] = arr[j] - arr[j + 1];
+                    arr[j] = arr[j] - arr[j + 1];
+                }
+            }
+        }
+        
+        for (int num : arr) {
+            System.out.print(num + " ");
+ }
+}
+}
